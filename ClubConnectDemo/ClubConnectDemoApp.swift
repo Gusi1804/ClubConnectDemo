a9aec2197c4a494837e8cb230de430427fe4e4dc
Gustavo Garfias <gustavo.egarfias@gmail.com>
2024-09-03 23:11:15 -0400

    Initial Commit

---
+++ ClubConnectDemo/ClubConnectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubConnectDemoApp.swift
+//  ClubConnectDemo
+//
+//  Created by Gustavo Garfias on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClubConnectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
